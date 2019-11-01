e1b0eaa575e10adf54fc4c7815bf5ecde5d5ab66
Nothile Ngcobo <nothilengcobo@nothiles-MacBook-Pro.local>
2019-11-01 16:14:18 +0200

    addded src code

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,28 +1,31 @@
-let password = 'Nothilebb789#';
+// let passwordIsOk = 'Nothilebb789#';
+let password = require('../src/passwordCheck');
 
 describe('password', function () {
-    it('should exist', function () {
-        expect(passwordIsOk).not.toBeNull();
-    });
+    var passwordString = "Nothilebb789#";
 
-    it('should have more than 8 charecters', function () {
-        expect(passwordIsOk.length).toBeGreaterThan(8);
-    });
+    // it('should exist', function () {
+    //     expect(passwordIsOk).not.toBeNull();
+    // });
 
-    it('should have uppercase', function () {
-        expect(passwordIsOk).toMatch(/[A-Z]/);
+    // it('should have more than 8 charecters', function () {
+    //     expect(passwordIsOk.length).toBeGreaterThan(8);
+    // });
+
+     it('should have uppercase', function () {
+         expect(passwordIsOk.password(passwordString)).toMatch(/[A-Z]/);
     });
 
     it('should have lowercase', function () {
-        expect(passwordIsOk).toMatch(/[a-z]/);
+         expect(password.passwordIsOk(passwordString)).toMatch(/[a-z]/);
     });
 
     it('should have a digit', function () {
-        expect(passwordIsOk).toMatch(/[0-9]/);
+        expect(password.passwordIsOk(passwordString)).toMatch(/[0-9]/);
     });
     it('should have special charecter', function () {
-        expect(passwordIsOk).toMatch(/[%,&.@,*.#,!,$]/);
-    });
+        expect(password.specialcharecter(passwordString)).toMatch(/[%,&.@,*.#,!,$]/);
+     });
 
 
 });
--- src/passwordCheck.js
+++ src/passwordCheck.js
@@ -1,7 +1,43 @@
-function charecter(){
-    
+function passwordIsOk(password) {
+    try {
+        var charecters = /^(?=.*[0-9])/;
+        if(password.match(charecters) == null) throw("Password must have at least one digit");
+        
+        return password;
+    } catch (err) {
+        return err;
+    }
 }
 
-module.exports={
-    charecter
+
+// function passwordIsOk(password) {
+//     try {
+//         var charecters = /^(?=.*[a-z])/;
+//         if(password.match(charecters) == null) throw("Password must have lowercase");
+        
+//         return password;
+//     } catch (err) {
+//         return err;
+//     }
+// }
+
+
+function specialcharecter(password) {
+    try {
+        var charecters = /^(?=.*[%,&.@,*.#,!,$])/;
+        if(password.match(charecters) == null) throw("Password must have at least one digit");
+        
+        return password;
+    } catch (err) {
+        return err;
+    }
 }
+// console.log(passwordIsOk('nsjdvsvd132'));
+
+
+    
+module.exports={
+    passwordIsOk,
+    specialcharecter
+}
+    
